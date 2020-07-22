49db5284d7255b6a0caad03f8d23a37b09817840
Aronnok093 <64925270+Aronnok093@users.noreply.github.com>
2020-07-22 22:29:39 +0600

    Add files via upload

---
+++ String/testing.c
@@ -0,0 +1,38 @@
+#include<stdio.h>
+int main() {
+   char str[50];
+   char n_srt[50];
+   int count=0,i=0,j=0,k;
+   printf("Enter something: \n");
+   scanf("%[^\t]s", &str);
+   printf("Given String:\n%s", str);
+   while(str[i]!='\0'){
+        if(str[i]=='\n'){
+            j=i;
+        }
+        i=i+1;
+    }
+    j=j-1;
+    while(str[j]!=str[0]){
+        j--;
+    }
+    while(str[j]!='\0'){
+        n_srt[i]=str[j];
+        j++;
+        i++;
+    }
+    count=i;
+    printf("\n");
+    while(count!=0){
+        count=count-1;
+        for(j=0;j<count;j++){
+            if('A'<=n_srt[j]&&n_srt[j]<='Z'||'a'<=n_srt[j]&&n_srt[j]<='z')
+            printf("%c",n_srt[j]);
+        }
+        printf("\n");
+    }
+
+
+}
+
+
