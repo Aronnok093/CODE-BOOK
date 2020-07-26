d4d6248442ccd50b25cc38a2d50def14ba13a728
Aronnok093 <64925270+Aronnok093@users.noreply.github.com>
2020-07-26 18:25:16 +0600

    Update Missing Number.py

--- Mathematics/Missing Number.py
+++ Mathematics/Missing Number.py
@@ -1,18 +1,4 @@
-term=int(input())
-li=list(map(int,input().split()))
-mini=min(li)
-high=max(li)
-flag=0
-if term not in li:
-    print(term)
-    exit(0)
-if len(li)==1:
-    if li[0]-1==0:
-        print(li[0]+1)
-    else:
-        print(li[0]-1)
-li.sort()
-for i in range(len(li)-1):
-    if li[i]+1!=li[i+1]:
-        print(li[i]+1)
-    
+n = int(input())
+line = [int(x) for x in input().split()]
+pattern = [i for i in range (1,n+1)]
+print(list(set(pattern) ^ set(line))[0])
