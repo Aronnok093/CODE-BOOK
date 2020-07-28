3569fc9ddf99489d7fd8f50bb78ec6f6f9ea4867
Aronnok093 <64925270+Aronnok093@users.noreply.github.com>
2020-07-28 13:14:52 +0600

    Update strickly increase.py

--- String/strickly increase.py
+++ String/strickly increase.py
@@ -1,4 +1,4 @@
-def checkIncreasingSequence(seq):
+def checkIncreasingSequence(seq): #use string as a input ex:checkIncreasingSequence("1234")
     for previous, next in zip(seq[:-1],seq[1:]):
         if not previous < next:
             return False
