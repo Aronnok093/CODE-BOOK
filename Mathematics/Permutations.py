1eaa885925ee4c4dd4b7da1fe3450c5ebc30ff21
Aronnok093 <64925270+Aronnok093@users.noreply.github.com>
2020-07-29 20:34:59 +0600

    Create Permutations.py

---
+++ Mathematics/Permutations.py
@@ -0,0 +1,18 @@
+num=int(input())
+ans=[]
+ans1=[]
+arr=[]
+for i in range(1,num+1):
+    if i%2==0:
+        ans.append(i)
+    else:
+        ans1.append(i)
+if num==1:
+    print(num)
+    exit(0)
+if abs(ans[-1]-ans1[0])==1:
+    print("NO SOLUTION")
+    exit(0)
+else:
+    arr=ans+ans1
+    print(*arr,)
