c519502997ccf322207501e9fcb617f71d151f72
Aronnok093 <64925270+Aronnok093@users.noreply.github.com>
2020-07-30 09:24:23 +0600

    Add files via upload

---
+++ Mathematics/Number Spiral.py
@@ -0,0 +1,20 @@
+term=int(input())
+while term !=0:
+    y,x=map(int,input().split())
+    if x > y:
+        if x%2==1:
+            ans=x*x-y+1
+        else:
+            x=x-1
+            ans=x*x+y
+        print(ans)
+    else :
+        if y%2==0:
+            ans=y*y-x+1
+        else:
+            y=y-1
+            ans=y*y+x
+        print(ans)
+
+    term=term-1
+        
