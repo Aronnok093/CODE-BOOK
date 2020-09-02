06d8a79c30b3ca75a8452d9f037d92387146fb85
Aronnok093 <fardin.aronock@yahoo.com>
2020-09-02 13:25:11 +0600

    Create not a palindrome, then add character after the string to convert it into a palindrome.c

---
+++ String/not a palindrome, then add character after the string to convert it into a palindrome.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include<string.h>
+int main()
+{
+  char text[100];
+  int begin, middle, end, length = 0;
+
+  gets(text);
+
+  while (text[length] != '\0')
+    length++;
+
+  end = length - 1;
+  middle = length/2;
+
+  for (begin = 0; begin < middle; begin++)
+  {
+    if (text[begin] != text[end])
+    {
+      printf("\n\nNot a palindrome.\n\n");
+      printf("palindrome text is:%s",text);
+      printf("%s\n\n",strrev(text));
+      break;
+    }
+    end--;
+  }
+  if (begin == middle)
+    printf("Palindrome.\n");
+
+  return 0;
+}
