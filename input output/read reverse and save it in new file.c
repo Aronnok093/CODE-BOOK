5c29251efe11012f1a9b09bb59c56720163fb490
Aronnok093 <fardin.aronock@yahoo.com>
2020-10-11 01:40:02 +0600

    Update read reverse and save it in new file.c

--- input output/read reverse and save it in new file.c
+++ input output/read reverse and save it in new file.c
@@ -1,1 +1,21 @@
 
+#include<stdio.h>
+void main()
+{
+    long loc;
+    char ch;
+    FILE *in, *out;
+    in = fopen("f1.txt", "rb");   // must create new file name f1.txt
+    out = fopen("f2.txt", "wb");
+    fseek( in,-1, SEEK_END);
+    loc = ftell(in);
+    while(loc >= 0 )
+    {
+        fseek(in, loc, SEEK_SET );
+        ch = fgetc(in);
+        fputc(ch, out);
+        loc--;
+    }
+    fclose(in);
+    fclose(out);
+}
