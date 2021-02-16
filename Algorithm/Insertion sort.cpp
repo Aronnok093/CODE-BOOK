42cdbdc560eaac56cd909b08f4ae8e0d75ec29e3
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2021-02-17 05:39:19 +0600

    Update Insertion sort.cpp

--- Algorithm/Insertion sort.cpp
+++ Algorithm/Insertion sort.cpp
@@ -18,3 +18,30 @@ int main(){
 
 
 }
+/*Time Complexity: O(n*2)
+
+Auxiliary Space: O(1)
+
+Boundary Cases: Insertion sort takes maximum time to sort if elements are sorted in reverse order. And it takes minimum time (Order of n) when elements are already sorted.
+
+Algorithmic Paradigm: Incremental Approach
+
+Sorting In Place: Yes
+
+Stable: Yes
+
+Online: Yes
+
+Uses: Insertion sort is used when number of elements is small. It can also be useful when input array is almost sorted, only few elements are misplaced in complete big array.
+
+What is Binary Insertion Sort?
+We can use binary search to reduce the number of comparisons in normal insertion sort. Binary Insertion Sort uses binary search to find the proper location to insert the selected item at each iteration. In normal insertion, sorting takes O(i) (at ith iteration) in worst case. We can reduce it to O(logi) by using binary search. The algorithm, as a whole, still has a running worst case running time of O(n2) because of the series of swaps required for each insertion. Refer this for implementation.
+
+How to implement Insertion Sort for Linked List?
+Below is simple insertion sort algorithm for linked list.
+
+1) Create an empty sorted (or result) list
+2) Traverse the given list, do following for every node.
+......a) Insert current node in sorted way in sorted or result list.
+3) Change head of given linked list to head of sorted (or result) list. */
+
