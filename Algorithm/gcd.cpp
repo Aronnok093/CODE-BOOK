ac31cd4da15298a8c9307bf0ed27efe330d5e941
MD. Fardin Jaman Aranyak <fardin.aronock@yahoo.com>
2023-02-19 13:37:58 +0600

    Update gcd.cpp

--- Algorithm/gcd.cpp
+++ Algorithm/gcd.cpp
@@ -2,17 +2,18 @@
 using namespace std;
 int gcd(int a, int b){
 
-    if(!b==0){
-        gcd(a,a%b);
-    }
-    return b;
+    if(b==0) return a;
+   
+    return gcd(b,a%b);
+    
+     
 }
 
 int main(){
-    #ifndef ONLINE_JUDGE
-    freopen("input.txt","r",stdin);
-    freopen("output.txt","w",stdout);
-    #endif
+    //#ifndef ONLINE_JUDGE
+    //freopen("input.txt","r",stdin);
+   //freopen("output.txt","w",stdout);
+    //#endif
 
     int t;
     cin>>t;
